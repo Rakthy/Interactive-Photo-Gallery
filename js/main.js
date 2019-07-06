3c20c5d7b9b78b172359eb7e19df5f380a1a8f32
Rakthy Thip <campinamerica@yahoo.com>
2019-07-05 22:52:10 -0700

    finally
    got the search filter to work!!  now I just have to figure out how to display it center.

--- js/main.js
+++ js/main.js
@@ -3,23 +3,20 @@
 let search = document.getElementById("search");
 search.addEventListener("keyup", filterImage);
 
-
 function filterImage() {
   let filter = document.getElementById("search").value.toLowerCase();
   let photoContainer = document.querySelectorAll(".photo-container");
-  [].forEach.call(photoContainer, function(photoContainer) {
+
+  //loop through all photo-containers
+  for (let i = 0; i < photoContainer.length; i++) {
     let a = document.getElementsByTagName("a");
     for (let i = 0; i < a.length; i++) {
-      let anchorFilter = a[i].getAttribute('data-title');
-      for(let i = 0; i < anchorFilter.length; i++){
-        console.log(anchorFilter[i]);
-
-      if (anchorFilter[i] != -1) {
-        photoContainer.style.display = "grid";
-      } else {
-        photoContainer.style.visibility = "hidden";
+      let anchorFilter = a[i].getAttribute("data-title");
+        if (anchorFilter.toLowerCase().indexOf(filter) > -1) {
+          photoContainer[i].style.display = "inline-block";
+        } else {
+          photoContainer[i].style.display = "none";
+        }
       }
     }
-    }
-  });
-}
+  }
