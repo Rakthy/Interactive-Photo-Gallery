ccabc940cb4b8f05260a195b388dea2ee39a4414
Rakthy Thip <campinamerica@yahoo.com>
2019-07-05 20:10:53 -0700

    Search Input
    search input attempt...

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 //filter input "search"
 
-let search = document.getElementById('search');
-search.addEventListener('keyup', filter);
-
-function filter() {
-  let filter = document.getElementById('search').value.toLowerCase();
-  let photoContainer = document.querySelectorAll('.photo-container');
-  for(let i = 0; i < photoContainer.length; i ++);
-let a = photoContainer[i].getElementsByTagName('a').attribute;
-  for(let i = 0; i < a.length; i ++);
-
-  if (a.toLowerCase().indexOf(filter) > -1){
-    photoContainer.style.display = 'grid';
-  } else {
-    photoContainer.style.visibility ='hidden';
-
-  }
+let search = document.getElementById("search");
+search.addEventListener("keyup", filterImage);
 
 
-	}
+function filterImage() {
+  let filter = document.getElementById("search").value.toLowerCase();
+  let photoContainer = document.querySelectorAll(".photo-container");
+  [].forEach.call(photoContainer, function(photoContainer) {
+    let a = document.getElementsByTagName("a");
+    for (let i = 0; i < a.length; i++) {
+      let anchorFilter = a[i].getAttribute("data-title");
+      console.log(anchorFilter);
+      if (anchorFilter.toLowerCase().indexOf(text) != -1) {
+        photoContainer.style.display = "grid";
+      } else {
+        photoContainer.style.visibility = "hidden";
+      }
+    }
+  });
+}
